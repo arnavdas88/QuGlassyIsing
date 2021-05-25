2198481178954e44e5ae8503d52a314ce800ba6e
arnavdas88 <arnav.das88@gmail.com>
2021-05-25 19:27:28 +0530

    1D Neghibour

--- cpm-vqe/interaction_helper.py
+++ cpm-vqe/interaction_helper.py
@@ -18,3 +18,66 @@ def reversed_kronecker(cell_1: int, cell_2: int):
         interaction_strength = j
         return interaction_strength
 
+class Point:
+    """Defines a point
+    """
+    def __init__(self, x, y, parent = [[]]):
+        """Creates a point instance on a parent
+
+        Args:
+            x (int): X coordinate of the point on a matrix
+            y (int): Y coordinate of the point on a matrix
+            parent (2D Array, optional): Parent matrix. Defaults to a blank matrix.
+        """
+        self.x = x
+        self.y = y
+        self.parent = parent
+    
+    @property
+    def top(self, ):
+        """Top point to the original
+
+        Returns:
+            Any: Point on the top
+        """
+        return self.parent[self.x-1][self.y]
+
+    @property
+    def bottom(self, ):
+        """Bottom point to the original
+
+        Returns:
+            Any: Point on the bottom
+        """
+        return self.parent[self.x+1][self.y]
+
+    @property
+    def left(self, ):
+        """Left point to the original
+
+        Returns:
+            Any: Point on the left
+        """
+        return self.parent[self.x][self.y-1]
+
+    @property
+    def right(self, ):
+        """Right point to the original
+
+        Returns:
+            Any: Point on the right
+        """
+        return self.parent[self.x][self.y+1]
+
+def get_1_neghibour(mat, point):
+    """Gets the 1D neghibour in respect to the specified point on the matrix  
+
+    Args:
+        mat (2D Array): The base matrix
+        point (int, int): x, y point
+
+    Returns:
+        Tuple: Returns 4 neghibouring points 
+    """
+    point = Point(*point, mat)
+    return ( point.left, point.right, point.top, point.bottom)
