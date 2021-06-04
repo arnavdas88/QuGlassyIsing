8904ed0b571d7d1fa14dc2ad5fb969cafa6019ca
arnavdas88 <arnav.das88@gmail.com>
2021-06-04 11:54:07 +0530

    Black formatted

--- qiskit_glassydynamics/helpers/ising.py
+++ qiskit_glassydynamics/helpers/ising.py
@@ -2,6 +2,7 @@ from .decoretors import str_to_hamiltonian
 import numpy as np
 from qiskit.opflow.primitive_ops.pauli_sum_op import PauliSumOp
 
+
 def get_interaction(element_1, element_2, matrix_dimension):
     """Gets singular interaction between 2 elements of an ising matrix and returns the hamiltonion
 
@@ -9,7 +10,7 @@ def get_interaction(element_1, element_2, matrix_dimension):
         element_1 ([int, int]): x, y coordinate of element 1 on the ising matrix
         element_2 ([int, int]): x, y coordinate of element 2 on the ising matrix
         matrix_dimension ([int, int]): Tuple representing the height, width of the ising matrix
-    
+
     Example:
         element_1 : [1, 0]
         element_2 : [2, 0]
@@ -27,29 +28,31 @@ def get_interaction(element_1, element_2, matrix_dimension):
     (R1, C1) = element_1
     (R2, C2) = element_2
     if (-1 in element_1) or (-1 in element_2) or (tR in [R1, R2]) or (tC in [C1, C2]):
-        # Return empty, for outside bounds in edge cases  
+        # Return empty, for outside bounds in edge cases
         return ""
 
     for _ in range(tR):
         R = []
         for _ in range(tC):
-            R += [ "I" ]
+            R += ["I"]
         output += [R]
-    R,C = element_1
+    R, C = element_1
     output[R][C] = "Z"
-    R,C = element_2
-    output[R][C] = "Z" 
-    output = "^".join([ "^".join([ R for R in C ]) for C in output ])
+    R, C = element_2
+    output[R][C] = "Z"
+    output = "^".join(["^".join([R for R in C]) for C in output])
     return f"({output})"
 
-def array2hamiltonian(arr, op = 'Z'):
+
+def array2hamiltonian(arr, op="Z"):
     result = []
     for i in arr:
-        result += [op if i == 1 else 'I']
+        result += [op if i == 1 else "I"]
     return f"({'^'.join(result)})"
 
+
 @str_to_hamiltonian
-def Ising2DHamiltonian(matrix_dimension = None) -> PauliSumOp:
+def Ising2DHamiltonian(matrix_dimension=None) -> PauliSumOp:
     """Gets total interaction over the ising matrix and returns the hamiltonian
 
     Args:
@@ -59,16 +62,16 @@ def Ising2DHamiltonian(matrix_dimension = None) -> PauliSumOp:
         PauliSumOp: PauliSumOp representation of the total hamiltonian.
     """
     if not matrix_dimension:
-        matrix_dimension = (3 , 3)
+        matrix_dimension = (3, 3)
     (tR, tC) = matrix_dimension
     output = ""
     for R in range(tR):
         for C in range(tC):
             Oi = (R, C)
-            Li = (R-1, C)
-            Ri = (R+1, C)
-            Ti = (R, C-1)
-            Bi = (R, C+1)
+            Li = (R - 1, C)
+            Ri = (R + 1, C)
+            Ti = (R, C - 1)
+            Bi = (R, C + 1)
             I = []
             I += [get_interaction(Li, Oi, matrix_dimension)]
             I += [get_interaction(Ri, Oi, matrix_dimension)]
@@ -77,11 +80,12 @@ def Ising2DHamiltonian(matrix_dimension = None) -> PauliSumOp:
             I = "+".join(I)
             # print(I)
             output += I + "+"
-    output = output.replace("++", "+").replace("++", "+").strip('+')
+    output = output.replace("++", "+").replace("++", "+").strip("+")
     return output
 
+
 @str_to_hamiltonian
-def Ising1DHamiltonian(matrix_length = None) -> PauliSumOp:
+def Ising1DHamiltonian(matrix_length=None) -> PauliSumOp:
     """Gets total interaction over the ising matrix and returns the hamiltonian
 
     Args:
@@ -97,13 +101,14 @@ def Ising1DHamiltonian(matrix_length = None) -> PauliSumOp:
     arr = np.asarray(arr)
     opmap = []
     for i in range(matrix_length):
-        opmap += [ array2hamiltonian(arr) ]
+        opmap += [array2hamiltonian(arr)]
         arr = np.roll(arr, 1)
     opmap = "+".join(opmap)
     return opmap
 
+
 @str_to_hamiltonian
-def FieldHamiltonian(matrix_dimension = None, constructor = 'Z')->PauliSumOp:
+def FieldHamiltonian(matrix_dimension=None, constructor="Z") -> PauliSumOp:
     """Generates a field hamiltonian for the overall ising.
 
     Args:
@@ -111,19 +116,19 @@ def FieldHamiltonian(matrix_dimension = None, constructor = 'Z')->PauliSumOp:
         constructor (str, optional): The interaction operation for the field hamiltonian. Defaults to 'Z'.
 
     Returns:
-        PauliSumOp: PauliSumOp representation of the field hamiltonian 
+        PauliSumOp: PauliSumOp representation of the field hamiltonian
     """
     if not matrix_dimension:
-        matrix_dimension = (3 , 3)
+        matrix_dimension = (3, 3)
     if type(matrix_dimension) == type(()):
         (tR, tC) = matrix_dimension
-        (tR, tC) = (tR*tC, tR*tC)
+        (tR, tC) = (tR * tC, tR * tC)
     if type(matrix_dimension) == int:
         (tR, tC) = (matrix_dimension, matrix_dimension)
     output = []
     for R in range(tR):
         r = []
         for C in range(tC):
-            r += [constructor if R==C else "I"]
+            r += [constructor if R == C else "I"]
         output += [f"({'^'.join(r)})"]
     return "+".join(output)
