ea5f9beb6f6088d85da577c65d4a7fe17070d129
Arnav Das <arnav.das88@gmail.com>
2022-09-02 19:48:16 +0530

    Update

--- qiskit_glassydynamics/helpers/decoretors.py
+++ qiskit_glassydynamics/helpers/decoretors.py
@@ -8,7 +8,7 @@ def str_to_hamiltonian(coeffs_i = 1):
     def wrapper(func):
         def action(*args, **kwargs):
             op = func(*args, **kwargs)
-            return eval(op)
-            # return PauliSumOp.from_list( [ (op, coeffs_i) ] )
+            # return eval(op)
+            return PauliSumOp.from_list( [ (op, coeffs_i) ] )
         return action
     return wrapper
