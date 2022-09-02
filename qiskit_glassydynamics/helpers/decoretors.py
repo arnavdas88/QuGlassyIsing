37caa1a757a75dd26b0b835528da981abe71b947
Arnav Das <arnav.das88@gmail.com>
2022-09-02 19:38:21 +0530

    PauliSumOp to SummedOp in str_to_hamiltonian

--- qiskit_glassydynamics/helpers/decoretors.py
+++ qiskit_glassydynamics/helpers/decoretors.py
@@ -1,4 +1,5 @@
 from qiskit.opflow.primitive_ops.pauli_sum_op import PauliSumOp
+from qiskit.opflow.list_ops import SummedOp
 from qiskit.opflow import OperatorBase
 from qiskit.opflow import Z, X, I  # Pauli Z, X matrices and identity
 
@@ -8,6 +9,7 @@ def str_to_hamiltonian(coeffs_i = 1):
         def action(*args, **kwargs):
             op = func(*args, **kwargs)
             # return eval(op)
-            return PauliSumOp.from_list( [ (op, coeffs_i) ] )
+            # return PauliSumOp.from_list( [ (op, coeffs_i) ] )
+            return SummedOp.from_list( [ (op, coeffs_i) ] )
         return action
     return wrapper
